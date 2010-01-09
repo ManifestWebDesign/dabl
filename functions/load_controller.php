6a34c8778d0110a79880f7afddaf4a499de6b6b3
Dan Blaisdell <dan@manifestwebdesign.com>
2010-01-09 14:03:04 -0800

    support for IndexController in subdirectories

--- functions/load_controller.php
+++ functions/load_controller.php
@@ -11,6 +11,7 @@ function load_controller($route){
 		$render_partial=true;
 		array_shift($params);
 	}
+	if(!$params)$params[] = DEFAULT_CONTROLLER;
 
 	$last = array_pop($params);
 	$extension = "html";
@@ -21,29 +22,48 @@ function load_controller($route){
 		$params[] = implode('.', $file_parts);
 	}
 
-	//get controller instance
 	$c_dir = ROOT.'controllers'.DIRECTORY_SEPARATOR;
 	$view_prefix = '';
 	$view_dir = '';
+	$instance = null;
 
 	foreach($params as $key => $segment){
 		$view_dir = strtolower($segment);
 		$c_class = str_replace(array('_', '-'), ' ', $segment);
 		$c_class = ucwords($c_class);
 		$c_class = str_replace(' ', '', $c_class).'Controller';
-		$c_class_file = $c_dir.DIRECTORY_SEPARATOR.$c_class.'.php';
+		$c_class_file = $c_dir.$c_class.'.php';
+
+		//check if file exists
 		if(file_exists($c_class_file)){
 			require_once $c_class_file;
 			unset($params[$key]);
 			$instance = new $c_class;
 			break;
 		}
-		if(is_dir($c_dir.$segment)){
-			$c_dir .= $segment.DIRECTORY_SEPARATOR;
-			$view_prefix .= $segment.DIRECTORY_SEPARATOR;
+
+		//check if the segment matches directory name
+		$c_dir .= $segment.DIRECTORY_SEPARATOR;
+		if(is_dir($c_dir)){
 			unset($params[$key]);
-			continue;
+			//if there are no segments left, and we continue, then we'll never load anything
+			//so only continue the loop if there is more to loop through
+			if($params){
+				$view_prefix .= $segment.DIRECTORY_SEPARATOR;
+				continue;
+			}
 		}
+
+		//fallback check if default index exists in directory
+		$alternate_c_class = ucwords(DEFAULT_CONTROLLER).'Controller';
+		$alternate_c_class_file = $c_dir.$alternate_c_class.'.php';
+		if(file_exists($alternate_c_class_file)){
+			require_once $alternate_c_class_file;
+			$instance = new $alternate_c_class;
+			break;
+		}
+
+		//we tried...but 404
 		file_not_found($route);
 	}
 
