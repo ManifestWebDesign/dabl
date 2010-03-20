c1fc4594a89f7a3b23ffbc0f1f71c1588dcb474f
Dan Blaisdell <dan@manifestwebdesign.com>
2010-03-20 16:53:27 -0700

    update to MSSQL applyLimit method

--- libraries/dabl/adapter/mssql/DBMSSQL.php
+++ libraries/dabl/adapter/mssql/DBMSSQL.php
@@ -15,7 +15,7 @@ class DBMSSQL extends DBAdapter {
 	 * @param	  in The string to transform to upper case.
 	 * @return	 The upper case string.
 	 */
-	function toUpperCase($in){
+	function toUpperCase($in) {
 		return "UPPER(" . $in . ")";
 	}
 
@@ -25,7 +25,7 @@ class DBMSSQL extends DBAdapter {
 	 * @param	  in The string whose case to ignore.
 	 * @return	 The string in a case that can be ignored.
 	 */
-	function ignoreCase($in){
+	function ignoreCase($in) {
 		return "UPPER(" . $in . ")";
 	}
 
@@ -36,7 +36,7 @@ class DBMSSQL extends DBAdapter {
 	 * @param	  string String to append.
 	 * @return	 string
 	 */
-	function concatString($s1, $s2){
+	function concatString($s1, $s2) {
 		return "($s1 + $s2)";
 	}
 
@@ -48,7 +48,7 @@ class DBMSSQL extends DBAdapter {
 	 * @param	  int Number of characters to extract.
 	 * @return	 string
 	 */
-	function subString($s, $pos, $len){
+	function subString($s, $pos, $len) {
 		return "SUBSTRING($s, $pos, $len)";
 	}
 
@@ -58,119 +58,111 @@ class DBMSSQL extends DBAdapter {
 	 * @param	  string String to calculate length of.
 	 * @return	 string
 	 */
-	function strLength($s){
+	function strLength($s) {
 		return "LEN($s)";
 	}
 
 	/**
 	 * @see		DBAdapter::quoteIdentifier()
 	 */
-	function quoteIdentifier($text){
+	function quoteIdentifier($text) {
 		return '[' . $text . ']';
 	}
 
 	/**
 	 * @see		DBAdapter::random()
 	 */
-	function random($seed = null){
+	function random($seed = null) {
 		return 'rand('.((int) $seed).')';
 	}
 
 	/**
-	* Simulated Limit/Offset
-	* This rewrites the $sql query to apply the offset and limit.
-	* @see		DBAdapter::applyLimit()
-	* @author	 Justin Carlson <justin.carlson@gmail.com>
-	*/
-	function applyLimit(&$sql, $offset, $limit){
+	 * Simulated Limit/Offset
+	 * This rewrites the $sql query to apply the offset and limit.
+	 * @see		DBAdapter::applyLimit()
+	 * @author	 Justin Carlson <justin.carlson@gmail.com>
+	 */
+	function applyLimit(&$sql, $offset, $limit) {
 		// make sure offset and limit are numeric
-		if (!is_numeric($offset) || !is_numeric($limit)){
-			throw new Exception("DBMSSQL ::applyLimit() expects a number for argument 2 and 3");
+		if (!is_numeric($offset) || !is_numeric($limit)) {
+			throw new Exception("DBMSSQL::applyLimit() expects a number for argument 2 and 3");
 		}
 
-		// obtain the original select statement
-		preg_match('/\A(.*)select(.*)from/si',$sql,$select_segment);
-		if (count($select_segment)>0){
-			$original_select = $select_segment[0];
-		} else {
-			throw new Exception("DBMSSQL ::applyLimit() could not locate the select statement at the start of the query. ");
+		//split the select and from clauses out of the original query
+		$selectSegment = array();
+		preg_match('/\Aselect(.*)from(.*)/si',$sql,$selectSegment);
+		if (count($selectSegment)==3) {
+			$selectStatement = trim($selectSegment[1]);
+			$fromStatement = trim($selectSegment[2]);
 		}
-		$modified_select = substr_replace($original_select, null, stristr($original_select,'select') , 6 );
+		else {
+			throw new Exception("DBMSSQL::applyLimit() could not locate the select statement at the start of the query. ");
+		}
 
-		// obtain the original order by clause, or create one if there isn't one
-		preg_match('/order by(.*)\Z/si',$sql,$order_segment);
-		if (count($order_segment)>0){
-			$order_by = $order_segment[0];
-		} else {
+		//handle the ORDER BY clause if present
+		$orderSegment = array();
+		preg_match('/order by(.*)\Z/si',$fromStatement,$orderSegment);
+		if (count($orderSegment)==2) {
+			//remove the ORDER BY from $sql
+			$fromStatement = trim(str_replace($orderSegment[0], '', $fromStatement));
+			//the ORDER BY clause is used in our inner select ROW_NUMBER() clause
+			$countColumn = trim($orderSegment[1]);
+		}
 
-			// no order by clause, if there are columns we can attempt to sort by the columns in the select statement
-			$select_items = split(',',$modified_select);
-			if (count($select_items)>0){
-				$item_number = 0;
-				$order_by = null;
-				while ($order_by === null && $item_number<count($select_items)){
-					if ($select_items[$item_number]!='*' && !strstr($select_items[$item_number],'(')){
-						$order_by = 'order by ' . $select_items[0] . ' asc';
-					}
-					$item_number++;
+		//setup inner and outer select selects
+		$innerSelect = '';
+		$outerSelect = '';
+		foreach(explode(', ',$selectStatement) as $selCol) {
+			@list($column,,$alias) = explode(' ', $selCol);
+			//make sure the current column isn't * or an aggregate
+			if ($column!='*' && !strstr($column,'(')) {
+				//we can use the first non-aggregate column for ROW_NUMBER() if it wasn't already set from an order by clause
+				if(!isset($countColumn)) {
+					$countColumn = $column;
 				}
-			}
-			if ($order_by === null){
-				throw new Exception("DBMSSQL ::applyLimit() could not locate the order by statement at the end of your query or any columns at the start of your query. ");
-			} else {
-				$sql.= ' ' . $order_by;
-			}
 
+				//add an alias to the inner select so all columns will be unique
+				$innerSelect .= $column." AS [$column],";
+
+				//use the alias in the outer select if one was present on the original select column
+				if(isset($alias)) {
+					$outerSelect .= "[$column] AS $alias,";
+				} else {
+					$outerSelect .= "[$column],";
+				}
+			} else {
+				//agregate columns must always have an alias clause
+				if(!isset($alias)) {
+					throw new Exception("DBMSSQL::applyLimit() requires aggregate columns to have an Alias clause");
+				}
+				//use the whole aggregate column in the inner select
+				$innerSelect .= "$selCol,";
+				//only add the alias for the aggregate to the outer select
+				$outerSelect .= "$alias,";
+			}
 		}
 
-		// remove the original select statement
-		$sql = str_replace($original_select , null, $sql);
-
-		/* modify the sort order by for paging */
-		$inverted_order = '';
-		$order_columns = split(',',str_ireplace('order by ','',$order_by));
-		$original_order_by = $order_by;
-		$order_by = '';
-		foreach ($order_columns as $column){
-			// strip "table." from order by columns
-			$column = array_reverse(split("\.",$column));
-			$column = $column[0];
-
-			// commas if we have multiple sort columns
-			if (strlen($inverted_order)>0){
-				$order_by.= ', ';
-				$inverted_order.=', ';
-			}
-
-			// put together order for paging wrapper
-			if (stristr($column,' desc')){
-				$order_by .= $column;
-				$inverted_order .= str_ireplace(' desc',' asc',$column);
-			} elseif (stristr($column,' asc')) {
-				$order_by .= $column;
-				$inverted_order .= str_ireplace(' asc',' desc',$column);
-			} else {
-				$order_by .= $column;
-				$inverted_order .= $column .' desc';
-			}
+		//check if we got this far and still don't have a viable column to user with ROW_NUMBER()
+		if(!isset($countColumn)) {
+			throw new Exception("DBMSSQL::applyLimit() requires an ORDER BY clause or at least one non-aggregate column in the select statement");
 		}
-		$order_by = 'order by ' . $order_by;
-		$inverted_order = 'order by ' . $inverted_order;
+
+		//ROW_NUMBER() starts at 1 not 0
+		$from = ($offset+1);
+		$to = ($limit+$offset);
+
+		//substring our select strings to get rid of the last comma and add our FROM and SELECT clauses
+		$innerSelect = "SELECT ROW_NUMBER() OVER(ORDER BY $countColumn) AS RowNumber, ".substr($innerSelect,0,-1).' FROM';
+		$outerSelect = 'SELECT '.substr($outerSelect,0,-1).' FROM';
 
 		// build the query
-		$offset = ($limit+$offset);
-		$modified_sql = 'select * from (';
-		$modified_sql.= 'select top '.$limit.' * from (';
-		$modified_sql.= 'select top '.$offset.' '.$modified_select.$sql;
-		$modified_sql.= ') deriveda '.$inverted_order.') derivedb '.$order_by;
-		$sql = $modified_sql;
-
+		$sql = "$outerSelect ($innerSelect $fromStatement) AS derivedb WHERE RowNumber BETWEEN $from AND $to";
 	}
-	
+
 	function lastInsertId() {
 		$query = "SELECT scope_identity() as ID";
 		$result = $this->query($query);
-		foreach($result as $r){
+		foreach($result as $r) {
 			return (int)$r['ID'];
 		}
 	}
