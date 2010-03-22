a4ae5547be4e44bd491c77dfe699ab56ac9acdf2
Dan Blaisdell <dan@manifestwebdesign.com>
2010-03-21 17:44:07 -0700

    print_r2 function for formatted print_r

---
+++ helpers/print_r2.php
@@ -0,0 +1,14 @@
+<?php
+
+/**
+ * Wraps output of print_r in <pre> tags to make it
+ * readable in a browser
+ * @param mixed $array
+ * @param bool $return
+ * @return string
+ */
+function print_r2($array, $return){
+	$string = '<pre>'.print_r($array, true).'</pre>';
+	if($return) return $string;
+	echo $string;
+}
