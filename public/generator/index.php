24fd09813f97babe4bd782318a6e25b23b97f4bb
Dan Blaisdell <dan@manifestwebdesign.com>
2010-04-01 22:11:37 -0700

    generator files use css

--- public/generator/index.php
+++ public/generator/index.php
@@ -1,4 +1,12 @@
 <?php require_once './init.php' ?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" dir="ltr">
+	<head>
+		<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+		<title><?= $title ?></title>
+		<link type="text/css" rel="stylesheet" href="<?php echo site_url('css/style.css') ?>" />
+	</head>
+	<body>
 <script>
 function checkAll(name, connection, checked){
 	var boxes = document.getElementsByTagName('input');
@@ -58,3 +66,5 @@ function checkAll(name, connection, checked){
 	</table>
 	<input type="submit" value="Generate Files!" />
 </form>
+	</body>
+</html>
