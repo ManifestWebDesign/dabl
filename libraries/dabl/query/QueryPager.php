50749a4faa29f53fc1a4030ef64ffffc5228c8cc
Dan Blaisdell <dan@manifestwebdesign.com>
2010-05-13 12:05:02 -0700

    update to QueryPager

--- libraries/dabl/query/QueryPager.php
+++ libraries/dabl/query/QueryPager.php
@@ -74,14 +74,14 @@ class QueryPager{
 	/**
 	 * Sets the current page number
 	 */
-	function setPageNum($pageNumber){
-		if(!is_numeric($pageNumber) || $pageNumber<1)
-			$pageNumber = 1;
+	function setPageNum($page_number){
+		if(!is_numeric($page_number) || $page_number<1)
+			$page_number = 1;
 		
-		if($pageNumber>$this->getPageCount())
+		if($page_number>$this->getPageCount())
 			$this->page = $this->getPageCount();
 		else
-			$this->page = $pageNumber;
+			$this->page = $page_number;
 	}
 	
 	/**
@@ -198,38 +198,84 @@ class QueryPager{
 		else return $q->doSelect();
 	}
 
+	function getPagerLinks($urlFormat, $limit = 20, $CSSClass='', $CSSClassCurrent='', $linkText=array()){
+		throw new Exception(__METHOD__.' is deprectated');
+	}
+
 	/**
 	 * Generates pagination html in the form of <a> tags
-	 * @param string $urlFormat The url to format paging links with, ie: /actu/index.html?page=$pageNum. $pageNum to be replaced with page number.
-	 * @param int $limit For limiting the number of page number links
+	 * @param string $urlFormat The url to format paging links with, ie: /actu/index.html?page=$page_num. $page_num to be replaced with page number.
+	 * @param int $page_limit For limiting the number of page number links
 	 * @param string $CSSClass The link tag of the others
 	 * @param string $CSSClassCurrent The link tag css class where we are
-	 * @param array  $linkText Ie: array("first" => "go to first page", "prev" => "previous", "next" => "next", "first" => "go to last page")
+	 * @param array  $labels Ie: array("first" => "go to first page", "prev" => "previous", "next" => "next", "first" => "go to last page")
 	 * @return string Html paging content
 	 */
-	function getPagerLinks($urlFormat, $limit = 20, $CSSClass='', $CSSClassCurrent='', $linkText=array()){
-		$currentpage = $this->getPageNum();
-		if($limit)
-			$nbpages = min(array($this->getPageCount(), $limit));
+	function getLinks($urlFormat, $page_limit = null, $labels=array()){
+		$default_labels = array(
+			'first' => '&laquo;',
+			'prev' => '&lsaquo;',
+			'next' => '&rsaquo;',
+			'last' => '&raquo;'
+		);
+		$labels = array_merge($default_labels, $labels);
+
+		$page_limit = intval($page_limit);
+		if(!$page_limit) $page_limit = 9;
+		$mid_page_limit = $page_limit >> 1;
+		$page = $this->getPageNum();
+		$count = $this->getPageCount();
+		$start = max(1, min($count-$page_limit, $page - $mid_page_limit));
+		$end = min($count, max($page_limit, $page + $mid_page_limit));
+		
 		$str = '';
+		if ($count !== 1):
+			$str .= <<<EOF
+		Page:
+EOF;
+			if ($page > 1):
+				$link = site_url(str_replace('$page_num', 1, $urlFormat));
+				$str .= <<<EOF
+			<a href="{$link}">{$labels['first']}</a>
+EOF;
 
-		if (@$linkText['first'] && !$this->isFirstPage())
-			$str .= '<a class="'.$CSSClass.'" href="'.str_replace('$pageNum', 1, $urlFormat).'">'.$linkText['first'].'</a>';
-
-		if (@$linkText['prev'] && !$this->isFirstPage())
-			$str .= '<a class="'.$CSSClass.'" href="'.str_replace('$pageNum', $this->getPrevPageNum(), $urlFormat).'">'.$linkText['prev'].'</a>';
-
-		for($i=1; $i<=$nbpages; $i++)
-			$str .= '<a class="'.($i==$currentpage ? $CSSClassCurrent : $CSSClass).'" href="'.str_replace('$pageNum', $i, $urlFormat).'">'.$i.'</a>';
-
-		if (@$linkText['next'] && !$this->isLastPage())
-			$str .= '<a class="'.$CSSClass.'" href="'.str_replace('$pageNum', $this->getNextPageNum(), $urlFormat).'">'.$linkText['next'].'</a>';
-
-		if (@$linkText['last'] && !$this->isLastPage())
-			$str .= '<a class="'.$CSSClass.'" href="'.str_replace('$pageNum', $nbpages, $urlFormat).'">'.$linkText['last'].'</a>';
+				$link = site_url(str_replace('$page_num', $page-1, $urlFormat));
+				$str .= <<<EOF
+			<a href="{$link}">{$labels['prev']}</a>
+EOF;
+			endif;
+			
+			for($i = $start; $i <= $end; ++$i):
+				$str .= <<<EOF
+EOF;
+				if ($i == $page):
+					$str .= <<<EOF
+			<span>{$i}</span>
+EOF;
+				else:
+					$link = site_url(str_replace('$page_num', $i, $urlFormat));
+					$str .= <<<EOF
+			<a href="{$link}">{$i}</a>
+EOF;
+				endif;
+				$str .= <<<EOF
+EOF;
+			endfor;
+			
+			if ($page < $count):
+				$link = site_url(str_replace('$page_num', $page + 1, $urlFormat));
+				$str .= <<<EOF
+			<a href="{$link}">{$labels['next']}</a>
+EOF;
+			
+				$link = site_url(str_replace('$page_num', $count, $urlFormat));
+				$str .= <<<EOF
+			<a href="{$link}">{$labels['last']}</a>
+EOF;
+			endif;
+		endif;
 
 		return $str;
 	}
 
 }
-?>
