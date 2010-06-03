71731e8069f6d4dba766006359b4011d7d80b604
Aaron Fellin <aaron@manifestwebdesign.com>
2010-06-03 09:19:57 -0700

    sane array handling in site_url() (refs #1295)
    also, documentation!

--- helpers/site_url.php
+++ helpers/site_url.php
@@ -1,11 +1,22 @@
 <?php
 
+/**
+ * Return a rooted path to the given URL.
+ * If the URL is already a complete URL (prefixed with http:// or
+ * https://), then the URL will be returned unmodified.
+ * If $url is an array, then the contents will be imploded.
+ *
+ * @param mixed $url URL or array of URL segments
+ * @param bool $version Append the modification time of the file to the URL
+ * @return string
+ * @author Dan Blaisdell
+ */
 function site_url($url = '', $version = false){
-	if(strpos($url, 'https://')===0 || strpos($url, 'http://')===0)
-		return $url;
-
 	if(is_array($url))
 		$url = implode('/', $url);
+
+	if(strpos($url, 'https://')===0 || strpos($url, 'http://')===0)
+		return $url;
 
 	if($version){
 		$path = ROOT.'/public/'.$url;
