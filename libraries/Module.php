860164694d16e951b1a5290f365b089075664ca7
Aaron Fellin <aaron@manifestwebdesign.com>
2010-09-09 22:19:35 -0700

    Module efficiency improvements
    Make import() do the heavy lifting, intsead of autoload()
    Reformatted and whitespace fixed
    todo: comments?

--- libraries/Module.php
+++ libraries/Module.php
@@ -5,35 +5,32 @@ class Module {
 	static $namespaces = array();
 	static $classes = array();
 
-	static function autoload ( $class_name ) {
-		$ds = DIRECTORY_SEPARATOR;
-
-		foreach ( self::$namespaces as $namespace ) {
-			$namespace_a = explode(':', $namespace);
-			// get the root module
-			$root = array_shift( $namespace_a );
-
-			// turn the namespace into a path
-			$ns_path = implode($ds, $namespace_a);
-			$class_path = MODULE::$root_modules[$root] . $ds . $ns_path . $ds . $class_name . '.php';
+	static function autoload($class_name) {
+		foreach(self::$namespaces as $namespace => &$namespace_a) {
+			$class_path = MODULE::$root_modules[$namespace_a[0]].DIRECTORY_SEPARATOR.
+				$namespace_a[1].DIRECTORY_SEPARATOR.
+				$class_name.'.php';
 
 			// require file if it exists
-			if ( is_file ( $class_path )){
+			if (is_file($class_path)) {
 				require_once $class_path;
 				return;
 			}
 		}
 	}
 
-	static function import ( $namespace ) {
-		if ( !in_array( $namespace, self::$namespaces ) ) self::$namespaces[] = $namespace;
+	static function import($namespace) {
+		$namespace_a = explode(':', $namespace);
+		$root = array_shift($namespace_a);
+		$ns_path = implode(DIRECTORY_SEPARATOR, $namespace_a);
+		self::$namespaces[$namespace] = array($root, $ns_path);
 	}
 
-	static function remove ( $namespace ) {
-		foreach (self::$namespaces as $key=>$ns) if ($ns == $namespace) unset(self::$namespaces[$key]);
+	static function remove($namespace) {
+		unset(self::$namespaces[$namespace]);
 	}
 
-	static function addRepository ( $name, $module_path ) {
+	static function addRepository($name, $module_path) {
 		self::$root_modules[$name] = rtrim($module_path, '\\/');
 	}
 }
