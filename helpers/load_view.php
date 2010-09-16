e2e5c30445d1da2cc8a3ae3cfe0f92575ced6a6f
Aaron Fellin <aaron@manifestwebdesign.com>
2010-09-16 15:14:13 -0700

    rewrite load_view to protect its own internal variables

--- helpers/load_view.php
+++ helpers/load_view.php
@@ -1,53 +1,76 @@
 <?php
 
 /**
+ * @package helpers
  * @param string $view
  * @param array $params
  * @param bool $return_output
  * @param string $output_format
  * @return string
  */
-function load_view($view = null, $params = array(), $return_output = false, $output_format = 'html'){
-	if($return_output)
-		ob_start();
+function load_view(
+	$view = null,
+	$params = array(),
+	$return_output = false,
+	$output_format = 'html'
+) {
 
-	switch($output_format){
+	$_ = array(
+		'view'          => $view,
+		'params'        => $params,
+		'return_output' => $return_output,
+		'output_format' => $output_format
+	);
+
+	if ($_['return_output']) {
+		ob_start();
+	}
+
+	switch ($_['output_format']) {
 		case 'json':
 			if (!headers_sent()) {
 				header('Content-type: application/json');
 			}
-			echo json_encode_all($params);
+			echo json_encode_all($_['params']);
 			break;
+
 		case 'xml':
 			if (!headers_sent()) {
 				header('Content-type: application/xml');
 			}
-			echo xml_encode_all($params);
+			echo xml_encode_all($_['params']);
 			break;
+
 		case 'html':
-			foreach($params as $var => $value)
-				$$var = $value;
+			foreach($_['params'] as $_var => $_value) {
+				if ('_'==$_var) throw new Exception('Attempting to overwrite $_ with view parameter');
+				$$_var = $_value;
+			}
 
-			$view = str_replace('\\', '/', $view);
-			$view = trim($view, '/');
-			$view = str_replace('/', DIRECTORY_SEPARATOR, $view);
+			$_['view'] = str_replace('\\', '/', $_['view']);
+			$_['view'] = trim($view, '/');
+			$_['view'] = str_replace('/', DIRECTORY_SEPARATOR, $_['view']);
 
-			if(is_dir(ROOT."views".DIRECTORY_SEPARATOR."$view"))
-				$view = "$view".DIRECTORY_SEPARATOR."index";
+			if(is_dir(ROOT.'views'.DIRECTORY_SEPARATOR.$_['view']))
+				$_['view'] = $_['view'].DIRECTORY_SEPARATOR."index";
 
-			$view = ROOT."views".DIRECTORY_SEPARATOR."$view.php";
+			$_['view'] = ROOT.'views'.DIRECTORY_SEPARATOR."{$_['view']}.php";
 
-			if(!is_file($view))
-				file_not_found($view);
+			if(!is_file($_['view']))
+				file_not_found($_['view']);
 
-			require $view;
+			require $_['view'];
 			break;
+
 		default:
-			throw new exception("The extension $output_format is not yet supported.");
-			file_not_found($view);
+			throw new exception("The extension {$_['output_format']} is not yet supported.");
+			file_not_found($_['view']);
 			break;
 	}
 
-	if($return_output)
+	if ($_['return_output']) {
 		return ob_get_clean();
+	} else {
+		ob_end_flush();
+	}
 }
