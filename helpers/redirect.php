053aa7cd1ed2eae80fe8e47dcabaef8db31840c1
Nathan Kerr <nathan@manifestwebdesign.com>
2010-12-31 11:06:57 -0800

    Redirect changes to allow a "false" redirect (redirect without stopping the currently processing script).

--- helpers/redirect.php
+++ helpers/redirect.php
@@ -1,6 +1,13 @@
 <?php
 
-function redirect($url = ''){
+function redirect($url = '', $die = true){
 	header('Location: '.site_url($url));
-	die;
+	if($die)
+		die();
+	header('Content-Length: 0',true);
+	header('Content-Type: text/html',true);
+	header('Connection: close');
+	flush();
+	session_write_close();
+	set_time_limit(0);
 }
