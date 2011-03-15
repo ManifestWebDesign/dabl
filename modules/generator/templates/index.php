1b7563eb6c05077e640609def3075e4976273ff6
Dan Blaisdell <dan@manifestwebdesign.com>
2011-03-15 03:56:33 -0700

    formatting

--- modules/generator/templates/index.php
+++ modules/generator/templates/index.php
@@ -1,6 +1,6 @@
 <h1>
 	<?php echo BaseGenerator::spaceTitleCase($plural) ?>
-	<a href="<?php echo "<?php echo site_url('".$plural_url."/edit') ?>" ?>" class="ui-state-default ui-corner-all ui-button-link" title="New <?php echo str_replace('_', ' ', ucfirst($single)) ?>">
+	<a href="<?php echo "<?php echo site_url('" . $plural_url . "/edit') ?>" ?>" class="ui-state-default ui-corner-all ui-button-link" title="New <?php echo str_replace('_', ' ', ucfirst($single)) ?>">
 		<span class="ui-icon ui-icon-plusthick"></span>New <?php echo str_replace('_', ' ', ucfirst($single)) ?>
 
 	</a>
@@ -8,5 +8,6 @@
 <?php echo '<?php echo $pager->getLinks($pageString) ?>' ?>
 
 <div class="ui-widget-content ui-corner-all">
-<?php echo '<?php load_view("'.$plural_url.'/grid", $params) ?>' ?>
+	<?php echo "<?php load_view('" . $plural_url . "/grid', \$params) ?>" ?>
+
 </div>
