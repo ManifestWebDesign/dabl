db7890ad3369a86c8776c2d115d5a3badd19123d
Dan Blaisdell <dan@manifestwebdesign.com>
2011-03-21 02:33:48 -0700

    updated index.php text

--- views/index.php
+++ views/index.php
@@ -1,26 +1,35 @@
-<h1>Congratulations</h1>
+<h1>Congratulations, you have DABL</h1>
 
 <p>
-	DABL appears to be handling your browser requests correctly.
+	To generate your models, views or controllers, point your browser to the 
+	<a href="<?php echo site_url('generator') ?>/">generator</a>.
 </p>
 
 <p>
-	Once you are ready to generate your models, views or controllers, point your browser to the <a href="<?php echo site_url('generator') ?>/">generator</a>.
+	For security reasons, we recommend password protecting or removing the generator scripts on 
+	your production website.
 </p>
 
 <p>
-	For security reasons, we recommend password protecting or removing the generator scripts on your production website.
-</p>
-
-<p>
-	To disable and remove the Model-View-Controller framework, delete the following files and directories:
+	To disable and remove the Model-View-Controller framework, delete the following files and 
+	directories:
 </p>
 
 <ul>
 	<li>controllers/</li>
 	<li>views/</li>
-	<li>helpers/(anything but strip_request_slashes.php can go)</li>
 	<li>.htaccess</li>
+	
+	<li>config/controllers.php</li>
+	<li>config/session.php</li>
+	<li>config/views.php</li>
+	
+	<li>modules/controllers/</li>
+	<li>modules/hooks/</li>
+	<li>modules/session/</li>
+	<li>modules/views/</li>
+	
+	<li>public/css/</li>
 	<li>public/.htaccess</li>
 	<li>public/index.php</li>
 </ul>
