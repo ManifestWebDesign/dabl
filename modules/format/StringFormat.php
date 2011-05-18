18c3ab3914dfda54a46ef76bc48a3d5f45222e39
Dan Blaisdell <dan@manifestwebdesign.com>
2011-05-17 21:30:17 -0700

    update to StringFormat

--- modules/format/StringFormat.php
+++ modules/format/StringFormat.php
@@ -6,24 +6,24 @@ class StringFormat {
 	 * @param string $string
 	 * @return string
 	 */
-	static function className($string){
-		return self::titleCase($string);
+	static function className($string) {
+		return self::titleCase(self::clean($string));
 	}
-	
+
 	/**
 	 * @param string $string
 	 * @return string
 	 */
-	static function classMethod($string){
-		return lcfirst(self::titleCase($string));
+	static function classMethod($string) {
+		return lcfirst(self::titleCase(self::clean($string)));
 	}
-	
+
 	/**
 	 * @param string $string
 	 * @return string
 	 */
-	static function classProperty($string){
-		return lcfirst(self::titleCase($string));
+	static function classProperty($string) {
+		return lcfirst(self::titleCase(self::clean($string)));
 	}
 
 	/**
@@ -32,7 +32,7 @@ class StringFormat {
 	 * @return string
 	 */
 	static function url($table_name) {
-		return str_replace('_', '-', self::variable(self::removeAccents($table_name)));
+		return str_replace('_', '-', self::variable($table_name));
 	}
 
 	/**
@@ -41,7 +41,7 @@ class StringFormat {
 	 * @return string
 	 */
 	static function pluralURL($table_name) {
-		return str_replace('_', '-', self::pluralVariable(self::removeAccents($table_name)));
+		return str_replace('_', '-', self::pluralVariable(self::clean($table_name)));
 	}
 
 	/**
@@ -50,7 +50,7 @@ class StringFormat {
 	 * @return string
 	 */
 	static function variable($table_name) {
-		return strtolower(join('_', self::getWords(self::removeAccents($table_name))));
+		return strtolower(join('_', self::getWords(self::clean($table_name))));
 	}
 
 	/**
@@ -60,7 +60,7 @@ class StringFormat {
 	 * @return string
 	 */
 	static function pluralVariable($table_name) {
-		return strtolower(join('_', self::getWords(self::plural(self::removeAccents($table_name)))));
+		return strtolower(join('_', self::getWords(self::plural(self::clean($table_name)))));
 	}
 
 	/**
@@ -68,8 +68,8 @@ class StringFormat {
 	 * @param string $string
 	 * @return string
 	 */
-	static function titleCase($string, $separator = '') {
-		return implode($separator, array_map('ucfirst', self::getWords($string)));
+	static function titleCase($string, $delimiter = '') {
+		return implode($delimiter, array_map('ucfirst', self::getWords($string)));
 	}
 
 	/**
@@ -135,4 +135,30 @@ class StringFormat {
 		$b = array('A', 'A', 'A', 'A', 'A', 'A', 'AE', 'C', 'E', 'E', 'E', 'E', 'I', 'I', 'I', 'I', 'D', 'N', 'O', 'O', 'O', 'O', 'O', 'O', 'U', 'U', 'U', 'U', 'Y', 's', 'a', 'a', 'a', 'a', 'a', 'a', 'ae', 'c', 'e', 'e', 'e', 'e', 'i', 'i', 'i', 'i', 'n', 'o', 'o', 'o', 'o', 'o', 'o', 'u', 'u', 'u', 'u', 'y', 'y', 'A', 'a', 'A', 'a', 'A', 'a', 'C', 'c', 'C', 'c', 'C', 'c', 'C', 'c', 'D', 'd', 'D', 'd', 'E', 'e', 'E', 'e', 'E', 'e', 'E', 'e', 'E', 'e', 'G', 'g', 'G', 'g', 'G', 'g', 'G', 'g', 'H', 'h', 'H', 'h', 'I', 'i', 'I', 'i', 'I', 'i', 'I', 'i', 'I', 'i', 'IJ', 'ij', 'J', 'j', 'K', 'k', 'L', 'l', 'L', 'l', 'L', 'l', 'L', 'l', 'l', 'l', 'N', 'n', 'N', 'n', 'N', 'n', 'n', 'O', 'o', 'O', 'o', 'O', 'o', 'OE', 'oe', 'R', 'r', 'R', 'r', 'R', 'r', 'S', 's', 'S', 's', 'S', 's', 'S', 's', 'T', 't', 'T', 't', 'T', 't', 'U', 'u', 'U', 'u', 'U', 'u', 'U', 'u', 'U', 'u', 'U', 'u', 'W', 'w', 'Y', 'y', 'Y', 'Z', 'z', 'Z', 'z', 'Z', 'z', 's', 'f', 'O', 'o', 'U', 'u', 'A', 'a', 'I', 'i', 'O', 'o', 'U', 'u', 'U', 'u', 'U', 'u', 'U', 'u', 'U', 'u', 'A', 'a', 'AE', 'ae', 'O', 'o');
 		return str_replace($a, $b, $str);
 	}
+
+	static function clean($str, $delimiter = '-') {
+		$str = self::removeAccents($str);
+
+		// replace punctuation with dashes
+		$str = str_replace(str_split("!@#$%^&*()_+={}[]:\";\|,./<>?\n "), '-', $str);
+
+		// remove all but letters, numbers and dashes
+		$str = preg_replace('/[^a-zA-Z0-9_-]/', '', $str);
+		
+		// remove trailing or leading dashes
+		$str = trim($str, '-');
+		
+		// remove any occurances of double dashes
+		do {
+			$before = $str;
+			$str = str_replace('--', '-', $str);
+		} while ($before != $str);
+
+		// replace dashes with delimiter if delimiter is not a dash
+		if ($delimiter !== '-') {
+			$str = str_replace('-', $delimiter, $str);
+		}
+		return $str;
+	}
+
 }
