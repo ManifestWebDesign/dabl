a1bc0ccd9c3f752da99c83d8522b482b6edba4ad
Dan Blaisdell <dan@manifestwebdesign.com>
2011-08-19 15:35:59 -0700

    less fancy .htacces

--- public/index.php
+++ public/index.php
@@ -5,7 +5,7 @@ require_once '../config.php';
 // string with url requested by visitor.  Usually in the form of:
 // controller/action/arg1/arg2?param1=value1
 // @see public/.htaccess
-$requested_route = @$_GET['url'];
+$requested_route = array_shift(explode('?', $_SERVER['REQUEST_URI'], 2));
 
 // handle the request with whatever Hooks have been set for that purpose
 // @see config/controllers.php
