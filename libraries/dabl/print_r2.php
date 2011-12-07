7dacfb61842f1b1f2f4d23333409058c9cabd2e6
Dan Blaisdell <dan@manifestwebdesign.com>
2011-12-07 09:32:44 -0800

    updated print_r2

--- libraries/dabl/print_r2.php
+++ libraries/dabl/print_r2.php
@@ -1,14 +1,16 @@
 <?php
 
 /**
- * Wraps output of print_r in <pre> tags to make it
- * readable in a browser
+ * Calls print_r, encodes html entities, and wraps the resulting string in <pre>
+ * tags to make it readable in a browser.
  * @param mixed $array
  * @param bool $return
  * @return string
  */
-function print_r2($array, $return = false){
-	$string = '<pre>'.print_r($array, true).'</pre>';
-	if($return) return $string;
+function print_r2($array, $return = false) {
+	$string = '<pre>' . htmlentities(print_r($array, true)) . '</pre>';
+	if ($return) {
+		return $string;
+	}
 	echo $string;
 }
