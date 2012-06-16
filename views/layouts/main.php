32f05aa547df3989d6b94731c0f977e54f75a13c
Dan Blaisdell <dan@manifestwebdesign.com>
2012-06-16 10:30:01 -0700

    live mouse events for ui rollovers

--- views/layouts/main.php
+++ views/layouts/main.php
@@ -9,11 +9,13 @@
 		<script language="Javascript" type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jqueryui/1.8.16/jquery-ui.min.js"></script>
 		<script>
 		$(function() {
-			$('li.ui-state-default, a.ui-state-default, input.ui-state-default, div.ui-state-default, span.ui-state-default').hover(function(){
-				$(this).addClass('ui-state-hover');
-			}, function(){
-				$(this).removeClass('ui-state-hover');
-			});
+			$('li.ui-state-default, a.ui-state-default, input.ui-state-default, div.ui-state-default, span.ui-state-default')
+				.live('mouseenter', function(){
+					$(this).addClass('ui-state-hover');
+				})
+				.live('mouseleave', function(){
+					$(this).removeClass('ui-state-hover');
+				});
 			$('span.ui-button-link').live('click', function(e){
 				if ($(e.target).is('span')) {
 					$(this).find('a, input').click();
