fb7b0948b298dce75e164df3273bced9a3ead7f5
Dan Blaisdell <dan@manifestwebdesign.com>
2013-03-13 00:09:37 -0700

    begin database schema

---
+++ migrations/initial-schema.sql
@@ -0,0 +1,82 @@
+SET default_with_oids = false;
+SET client_encoding = 'UTF8';
+
+CREATE TABLE entity
+(
+	id serial PRIMARY KEY,
+	created timestamp with time zone,
+	updated timestamp with time zone
+);
+
+CREATE TABLE profile
+(
+	company_name character varying(255),
+	first_name character varying(255),
+	last_name character varying(255),
+	PRIMARY KEY (id)
+)
+INHERITS (entity);
+
+CREATE TABLE "user"
+(
+	profile_id integer NOT NULL PRIMARY KEY,
+	created timestamp with time zone,
+	updated timestamp with time zone,
+	email character varying(255),
+	"password" character varying(255),
+	FOREIGN KEY (profile_id) REFERENCES profile (id) ON UPDATE CASCADE ON DELETE CASCADE
+);
+
+CREATE TABLE "role" (
+	label character varying(255) NOT NULL,
+	"global" boolean NOT NULL DEFAULT false,
+	PRIMARY KEY (id)
+)
+INHERITS (entity);
+
+CREATE TABLE role_permission (
+	role_id integer NOT NULL,
+	permission_id smallint NOT NULL,
+	PRIMARY KEY (role_id, permission_id)
+);
+
+CREATE TABLE profile_member
+(
+	profile_id integer NOT NULL,
+	member_profile_id integer NOT NULL,
+	role_id integer NOT NULL,
+	created timestamp with time zone,
+	updated timestamp with time zone,
+	PRIMARY KEY (profile_id, member_profile_id, role_id),
+	FOREIGN KEY (member_profile_id) REFERENCES profile (id) ON UPDATE CASCADE ON DELETE CASCADE,
+	FOREIGN KEY (profile_id) REFERENCES profile (id) ON UPDATE CASCADE ON DELETE CASCADE,
+	FOREIGN KEY (role_id) REFERENCES "role" (id) ON UPDATE CASCADE ON DELETE RESTRICT
+);
+
+CREATE TABLE water_supplier
+(
+	profile_id integer NOT NULL PRIMARY KEY,
+	created timestamp with time zone,
+	updated timestamp with time zone,
+	FOREIGN KEY (profile_id) REFERENCES profile (id) ON UPDATE CASCADE ON DELETE CASCADE
+);
+
+CREATE TABLE water_tester
+(
+   profile_id integer NOT NULL PRIMARY KEY,
+   created timestamp with time zone,
+   updated timestamp with time zone,
+   gauge_number character varying(255),
+   gauge_calibrated date,
+   certification_number character varying(255),
+   FOREIGN KEY (profile_id) REFERENCES profile (id) ON UPDATE CASCADE ON DELETE CASCADE
+);
+
+ALTER TABLE water_tester OWNER TO molitech_oms;
+ALTER TABLE water_supplier OWNER TO molitech_oms;
+ALTER TABLE profile_member OWNER TO molitech_oms;
+ALTER TABLE "role" OWNER TO molitech_oms;
+ALTER TABLE "role_permission" OWNER TO molitech_oms;
+ALTER TABLE "user" OWNER TO molitech_oms;
+ALTER TABLE entity OWNER TO molitech_oms;
+ALTER TABLE profile OWNER TO molitech_oms;
