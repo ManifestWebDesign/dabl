0b21c712864842b8895ed56e593d1c37a1dccd0d
Dan Blaisdell <dan@manifestwebdesign.com>
2013-03-16 15:00:11 -0700

    bring back global.js

--- views/layouts/main.php
+++ views/layouts/main.php
@@ -7,6 +7,7 @@
 		<link type="text/css" rel="stylesheet" href="<?php echo site_url('css/themes/light-green/jquery-ui-1.8.custom.css', true) ?>" />
 		<script language="Javascript" type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js"></script>
 		<script language="Javascript" type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jqueryui/1.8.18/jquery-ui.min.js"></script>
+		<script language="Javascript" type="text/javascript" src="<?php echo site_url('js/global.js', true) ?>"></script>
 	</head>
 	<body>
 
