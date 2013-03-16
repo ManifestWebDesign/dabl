19e486185f4c16998e82ab08a58ff6aa31800713
Dan Blaisdell <dan@manifestwebdesign.com>
2013-03-16 13:54:57 -0700

    tabs style fix

--- views/layouts/main.php
+++ views/layouts/main.php
@@ -1,5 +1,5 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml" dir="ltr">
+<!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/xhtml" dir="ltr" ng-app="Main">
 	<head>
 		<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
 		<title><?php echo $title ?></title>
@@ -7,14 +7,18 @@
 		<link type="text/css" rel="stylesheet" href="<?php echo site_url('css/themes/light-green/jquery-ui-1.8.custom.css', true) ?>" />
 		<script language="Javascript" type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js"></script>
 		<script language="Javascript" type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jqueryui/1.8.18/jquery-ui.min.js"></script>
+		<script language="Javascript" type="text/javascript" src="<?php echo site_url('js/dabl.js', true) ?>"></script>
+		<script language="Javascript" type="text/javascript" src="<?php echo site_url('js/angular.js', true) ?>"></script>
+		<script language="Javascript" type="text/javascript" src="<?php echo site_url('js/angular-resource.js', true) ?>"></script>
 		<script language="Javascript" type="text/javascript" src="<?php echo site_url('js/global.js', true) ?>"></script>
+		<script language="Javascript" type="text/javascript" src="<?php echo site_url('js/models.js', true) ?>"></script>
 	</head>
-	<body>
+	<body ng-controller="MainController">
 
 		<div class="ui-tabs ui-widget ui-widget-header">
 			<ul class="ui-tabs-nav ui-helper-reset ui-helper-clearfix">
 			<?php foreach($actions as $label => $url): ?>
-				<li class="ui-state-default ui-corner-top <?php if (@$current_page == $label) echo "ui-tabs-selected ui-state-active ui-state-hover"?>">
+				<li class="ui-state-default ui-corner-top <?php if (@$current_page == $label) echo "ui-tabs-selected ui-tabs-active ui-state-active"?>">
 					<a href="<?php echo $url ?>"><?php echo $label ?></a>
 				</li>
 			<?php endforeach ?>
