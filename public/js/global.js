cb96cf53fab785f0d378d7685931529b7325ffbf
Dan Blaisdell <dan@manifestwebdesign.com>
2013-10-04 11:31:52 -0700

    better global.js

--- public/js/global.js
+++ public/js/global.js
@@ -1,35 +1,46 @@
 (function($){
+	window.initWidgets = function initWidgets($element) {
+		$element = $element || $('body');
 
-	$(function() {
-		$('.button-group').buttonset();
-		$('.button').each(function(){
+		$element.find('.button-group').buttonset();
+		$element.find('.button').each(function(){
 			var $btn = $(this),
-				icon = $btn.attr('data-icon');
+				icon = $btn.attr('data-icon'),
+				hasText = $btn.text().replace(/^\s+|\s+$/g,'') !== '' || $btn.find('input').length !== 0;
+
+			if (!hasText) {
+				$btn.text('&nbsp;');
+			}
 			if (icon) {
 				$btn.button({
 					icons: {
 						primary: 'ui-icon-' + icon
-					}
+					},
+					text: hasText
 				});
 			} else {
 				$btn.button();
 			}
 		});
 
-		$('li.ui-state-default, a.ui-state-default, input.ui-state-default, div.ui-state-default, span.ui-state-default')
+		$element.find('li.ui-state-default, a.ui-state-default, input.ui-state-default, div.ui-state-default, span.ui-state-default')
 			.on('mouseenter', function(){
 				$(this).addClass('ui-state-hover');
 			})
 			.on('mouseleave', function(){
 				$(this).removeClass('ui-state-hover');
 			});
-		$('span.button').on('click', function(e){
+		$element.find('span.button').on('click', function(e){
 			if ($(e.target).is('span')) {
 				$(this).find('a, input').click();
 			}
 		});
 
-		$('input.datepicker').datepicker();
-	});
+		$element.find('input.datepicker').datepicker();
+		$element.find('.tooltip[title]').tooltip();
+	};
 
+	$(function(){
+		initWidgets();
+	});
 })(jQuery);
