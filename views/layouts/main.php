bb8ef7bc27c860f6ad523ba3adad41e09d80c00e
Dan Blaisdell <dan@manifestwebdesign.com>
2014-06-05 00:37:56 -0700

    update jquery ui version

--- views/layouts/main.php
+++ views/layouts/main.php
@@ -10,7 +10,7 @@
 		<link type="text/css" rel="stylesheet" href="<?php echo site_url('css/style.css', true) ?>" />
 
 		<script language="Javascript" type="text/javascript" src="//ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
-		<script language="Javascript" type="text/javascript" src="//ajax.googleapis.com/ajax/libs/jqueryui/1.9.2/jquery-ui.min.js"></script>
+		<script language="Javascript" type="text/javascript" src="//ajax.googleapis.com/ajax/libs/jqueryui/1.10.4/jquery-ui.min.js"></script>
 		<script language="Javascript" type="text/javascript" src="<?php echo site_url('js/global.js', true) ?>"></script>
 	</head>
 	<body>
